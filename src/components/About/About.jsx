ce1da5967ee3025c822bf2851b374d3f4d67c065
futug <102650930+futug@users.noreply.github.com>
2023-08-01 22:37:32 +0600

    scroll animation tests

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,9 @@ import styles from "./About.module.css";
 import { AiOutlineInstagram, AiOutlineYoutube } from "react-icons/ai";
 import { useTranslation } from "react-i18next";
 
-const About = ({ timeline, ease, about }) => {
+import { easeInOut, motion, transform } from "framer-motion";
+
+const About = ({ timeline, ease }) => {
     let mainTitle = useRef();
     let mainSubtitle = useRef();
     let socialIconInst = useRef();
@@ -11,79 +13,128 @@ const About = ({ timeline, ease, about }) => {
     let photo = useRef();
     let rightBlock = useRef();
 
-    useEffect(() => {
-        timeline.from(mainTitle.current, 1, {
+    // useEffect(() => {
+    //     timeline.from(mainTitle.current, 1, {
+    //         opacity: 0,
+    //         x: -100,
+    //     });
+    //     timeline.from(mainSubtitle.current, 1, {
+    //         opacity: 0,
+    //         x: 100,
+    //     });
+    //     timeline.from([socialIconInst.current, socialIconTube.current], 1, {
+    //         opacity: 0,
+    //         y: 100,
+    //         stagger: {
+    //             amount: 0.4,
+    //         },
+    //         ease: ease,
+    //     });
+    //     timeline.from([photo.current, rightBlock.current], 1, {
+    //         opacity: 0,
+    //         x: 100,
+    //     });
+    // }, []);
+
+    const fadeLeft = {
+        hidden: {
             opacity: 0,
             x: -100,
-        });
-        timeline.from(mainSubtitle.current, 1, {
+        },
+
+        visible: (custom) => ({
+            opacity: 1,
+            x: 0,
+            transition: {
+                delay: custom * 0.2,
+                duration: 1,
+                ease: "easeInOut",
+            },
+        }),
+    };
+
+    const fadeRight = {
+        hidden: {
             opacity: 0,
             x: 100,
-        });
-        timeline.from([socialIconInst.current, socialIconTube.current], 1, {
+        },
+        visible: (custom) => ({
+            opacity: 1,
+            x: 0,
+            transition: {
+                delay: custom * 0.2,
+                duration: 1,
+                ease: "easeInOut",
+            },
+        }),
+    };
+
+    const fadeBottom = {
+        hidden: {
             opacity: 0,
             y: 100,
-            stagger: {
-                amount: 0.4,
+        },
+        visible: (custom) => ({
+            opacity: 1,
+            y: 0,
+            transition: {
+                delay: custom * 0.2,
+                duration: 1,
+                ease: "easeInOut",
             },
-            ease: ease,
-        });
-        timeline.from([photo.current, rightBlock.current], 1, {
-            opacity: 0,
-            x: 100,
-        });
-    }, []);
+        }),
+    };
 
     const { t } = useTranslation();
 
     return (
-        <section className={styles.about} id="about">
+        <motion.section initial="hidden" whileInView="visible" viewport={{ amount: 0.5 }} className={styles.about} id="about">
             <div className="container">
                 <div className={styles.aboutWrapper}>
                     <div className={styles.titleGroup}>
-                        <p ref={mainTitle} className={styles.title}>
+                        <motion.p variants={fadeLeft} custom={1} className={styles.title}>
                             {t("aboutTitle")}
-                        </p>
-                        <h1 ref={mainSubtitle} className={styles.subtitle}>
+                        </motion.p>
+                        <motion.h1 variants={fadeRight} custom={2} ref={mainSubtitle} className={styles.subtitle}>
                             {t("aboutSubTitle")}
-                        </h1>
+                        </motion.h1>
                     </div>
                     <div className={styles.content}>
                         <div className={styles.rightSideWrapper}>
                             <div className={styles.socialIcons}>
-                                <div ref={socialIconInst}>
+                                <motion.div variants={fadeBottom} custom={1}>
                                     <a href="https://www.instagram.com/etozhemazei/" target="_blank">
                                         <div className={`${styles.icoBorder} ${styles.icoBorderInst}`}>
                                             <AiOutlineInstagram className={styles.icon} size={30} />
                                         </div>
                                     </a>
-                                </div>
-                                <div ref={socialIconTube}>
+                                </motion.div>
+                                <motion.div variants={fadeBottom} custom={2}>
                                     <a href="https://www.youtube.com/channel/UCRPvpSnoK7LggA4mnjwRVog" target="_blank">
                                         <div className={`${styles.icoBorder} ${styles.icoBorderYouTube}`}>
                                             <AiOutlineYoutube className={`${styles.icon} ${styles.iconYouTube}`} size={30} />
                                         </div>
                                     </a>
-                                </div>
+                                </motion.div>
                             </div>
                             <div></div>
-                            <picture ref={photo} className={styles.photoWrapper}>
+                            <motion.picture variants={fadeRight} className={styles.photoWrapper}>
                                 <source className={styles.photo} srcSet="./authorphoto.webp" type="image/webp" />
                                 <source className={styles.photo} srcSet="./authorphoto.jpg" type="image/jpeg" />
                                 <img className={styles.photo} src="./authorphoto.jpg" alt="About" />
-                            </picture>
+                            </motion.picture>
                         </div>
-                        <div ref={rightBlock} className={styles.leftSideWrapper}>
+                        <motion.div variants={fadeRight} className={styles.leftSideWrapper}>
                             <p className={styles.name}>{t("aboutName")}:</p>
                             <p className={styles.description}>{t("aboutDescOne")}</p>
                             <p className={styles.description}>{t("aboutDescTwo")}</p>
                             <p className={styles.description}>{t("aboutDescThree")}</p>
                             <p className={styles.description}>{t("aboutDescFour")}</p>
-                        </div>
+                        </motion.div>
                     </div>
                 </div>
             </div>
-        </section>
+        </motion.section>
     );
 };
 
